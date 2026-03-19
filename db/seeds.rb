ddfad0120de447006c70d4ede9e194d64ee66799
steven roekens <stevenroekens@gmail.com>
2026-03-19 23:28:02 +0000

    index and show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.create!(
+  name: 'A cupboard under the stairs',
+  address: '4 Privet Drive, Little Whinging, Surrey, England',
+  description: 'A lovely summer feel for this spacious cupboard room. One small room, lots of storage and a friendly spider',
+  price_per_night: 75,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Entire apartment in North District',
+  address: 'North District, Taichung City, Taiwan',
+  description: 'Vinyl records atmosphere homestay',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+
+
+Flat.create!(
+  name: 'Lemeng Hotel (Beijing Houhai Drum Tower)',
+  address: 'No. 2 Xitao Hutong, Jiugulou Street, Xicheng District, Beijing, China',
+  description: 'The Lemeng Hotel (Beijing Houhai Drum Tower) is an ideal spot for travelers wanting to discover the city',
+  price_per_night: 35,
+  number_of_guests: 2
+)
