7aeff4ca304220175053f4849c272056c3c4d20f
steven roekens <stevenroekens@gmail.com>
2026-03-20 15:10:20 +0100

    finish CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "flats", to: "flats#index"
+  get "flats/new", to: "flats#new"
+  post "flats", to: "flats#create"
+  get "flats/:id/edit", to: "flats#edit", as: :edit_flat
+  patch "flats/:id", to: "flats#update"
+  delete "flats/:id", to: "flats#destroy"
   get "flats/:id", to: "flats#show", as: :flat
 end
